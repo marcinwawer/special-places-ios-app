264232fa34eb04aa1611994171ca1ab3b71f4d2d
Marcin Wawer <marcinwaw_git@icloud.com>
2024-08-07 13:13:25 +0200

    [feature] add DataServices with locations

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Marcin Wawer on 07-08-2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
