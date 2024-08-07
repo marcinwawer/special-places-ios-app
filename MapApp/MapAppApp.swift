6290613d419ad7f7e3189f6ce2978fa423db8e2d
Marcin Wawer <marcinwaw_git@icloud.com>
2024-08-07 12:42:05 +0200

    initial commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Marcin Wawer on 07-08-2024.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
