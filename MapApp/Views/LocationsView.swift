2417e10864e25aa2e44bc8cd9abcfab9c8a291da
Marcin Wawer <marcinwaw_git@icloud.com>
2024-08-12 21:40:35 +0200

    [feature] adapting ui for ipad

--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -11,13 +11,17 @@ import MapKit
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
             mapLayer
             
             VStack(spacing: 0) {
-                header.padding()
+                header
+                    .padding()
+                    .frame(maxWidth: maxWidthForIpad)
+                
                 Spacer()
                 locationsPreviewStack
             }
@@ -91,6 +95,8 @@ extension LocationsView {
                     LocationPreviewView(location: location)
                         .shadow(color: .black.opacity(0.3), radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition(.asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading))
